c2b1b2260b8b16b98974fd0f8e85542a7a732c00
babby <bobyif.if@gmail.com>
2020-08-31 03:13:58 +0300

    init

--- sqlite3/sqlite_lerning.py
+++ sqlite3/sqlite_lerning.py
@@ -3,6 +3,6 @@ import sqlite3
     conn = sqlite3.connect("lite.db")
     cur = conn.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTIGER, price REAL)")
-    cur.execute("INSERT INTO store VALUES ('Wine Glass', 8, 10.8)")
+    cur.execute("INSERT INTO store VALUES ('Wine Glass', 8, 10.9)")
     conn.commit()
     conn.close()

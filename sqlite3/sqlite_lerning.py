7b0036148f536cde12acbef1782a61b2ba10c3c2
bobyif <bobyif.if@gmail.com>
2020-09-19 18:00:05 +0300

    init

--- sqlite3/sqlite_lerning.py
+++ sqlite3/sqlite_lerning.py
@@ -1,15 +1,21 @@
-import pyscopg2
+import psycopg2
 
-def create_table(item,quantity,price):
-    conn = sqlite3.connect("lite.db")
+def create_table():
+    conn = psycopg2.connect("dbname= 'database1' user= 'postgres' password= 'fosatti' host = 'localhost' port='5432'")
     cur = conn.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTIGER, price REAL)")
+    conn.commit()
+    conn.close()
+
+def insurt(item,quantity,price):
+    conn = psycopg2.connect("lite.db")
+    cur = conn.cursor()
     cur.execute("INSERT INTO store VALUES(?,?,?)",(item,quantity,price))
     conn.commit()
     conn.close()
 
 def view():
-    conn = sqlite3.connect("lite.db")
+    conn = psycopg2.connect("lite.db")
     cur = conn.cursor()
     cur.execute("SELECT * FROM store")
     rows = cur.fetchall()
@@ -17,20 +23,21 @@ def view():
     return rows 
 
 def delete(item):
-    conn = sqlite3.connect("lite.db")
+    conn = psycopg2.connect("lite.db")
     cur = conn.cursor()
     cur.execute("DELETE FROM store WHERE item=?",(item,))
     conn.commit()
     conn.close()
 
 def uploade(quantity,item,price):
-    conn = sqlite3.connect("lite.db")
+    conn = psycopg2.connect("lite.db")
     cur = conn.cursor()
     cur.execute("UPDATE store SET quantity=?, item=? WHERE price=?",(quantity,item,price))
     conn.commit()
     conn.close()
 
+create_table()
 #uploade(27, "Chocolate", 5.59)
 #delete("Coffe Cup")
-print(view())
-create_table("cake", 18, 4.79)
+#print(view())
+#create_table("cake", 18, 4.79)

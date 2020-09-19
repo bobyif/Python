090743afbfb4a5fcabbaab5b93e6c02a2c767252
babby <bobyif.if@gmail.com>
2020-09-19 18:33:27 +0300

    Initial commit

--- pyscopg2/sqlite_lerning_try.py
+++ pyscopg2/sqlite_lerning_try.py
@@ -8,10 +8,10 @@ def create_table(names ,ages, lastname, networth):
     conn.commit()
     conn.close()
 
-name = input("write your first name : ")
-age = input("write your age : ")
-last_name = input("write your last name : ")
-networth = input("write your networth : ")
+name = input("write your first name? : ")
+age = input("write your age? : ")
+last_name = input("write your last name? : ")
+networth = input("write your networth? : ")
 
 create_table(name , age, last_name, networth)
 

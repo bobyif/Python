63cb8b21b2cf60b14457e2e02362b6b7bcb64646
bobyif <bobyif.if@gmail.com>
2020-09-22 23:45:39 +0300

    finished

--- sqlite3/sqlite_lerning.py
+++ sqlite3/sqlite_lerning.py
@@ -1,21 +1,21 @@
 import psycopg2
 
 def create_table():
-    conn = psycopg2.connect("dbname= 'database1' user= 'postgres' password= 'fosatti' host = 'localhost' port='5432'")
+    conn = psycopg2.connect("dbname= 'database 1' user= 'postgres' password= 'fosatti' host = 'localhost' port='5432'")
     cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTIGER, price REAL)")
+    cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)")
     conn.commit()
     conn.close()
 
-def insurt(item,quantity,price):
-    conn = psycopg2.connect("lite.db")
+def insert(item,quantity,price):
+    conn = psycopg2.connect("dbname= 'database 1' user= 'postgres' password= 'fosatti' host = 'localhost' port='5432'")
     cur = conn.cursor()
-    cur.execute("INSERT INTO store VALUES(?,?,?)",(item,quantity,price))
+    cur.execute("INSERT INTO store VALUES(%s,%s,%s)", (item,quantity,price))
     conn.commit()
     conn.close()
 
 def view():
-    conn = psycopg2.connect("lite.db")
+    conn = psycopg2.connect("dbname= 'database 1' user= 'postgres' password= 'fosatti' host = 'localhost' port='5432'")
     cur = conn.cursor()
     cur.execute("SELECT * FROM store")
     rows = cur.fetchall()
@@ -23,21 +23,22 @@ def view():
     return rows 
 
 def delete(item):
-    conn = psycopg2.connect("lite.db")
+    conn = psycopg2.connect("dbname= 'database 1' user= 'postgres' password= 'fosatti' host = 'localhost' port='5432'")
     cur = conn.cursor()
-    cur.execute("DELETE FROM store WHERE item=?",(item,))
+    cur.execute("DELETE FROM store WHERE item=%s", (item,))
     conn.commit()
     conn.close()
 
-def uploade(quantity,item,price):
-    conn = psycopg2.connect("lite.db")
+def update(quantity,price,item):
+    conn = psycopg2.connect("dbname= 'database 1' user= 'postgres' password= 'fosatti' host = 'localhost' port='5432'")
     cur = conn.cursor()
-    cur.execute("UPDATE store SET quantity=?, item=? WHERE price=?",(quantity,item,price))
+    cur.execute("UPDATE store SET quantity = %s, price= %s WHERE item = %s", (quantity,price,item))
     conn.commit()
     conn.close()
 
 create_table()
-uploade(27, "Chocolate", 5.59)
-#delete("Coffe Cup")
-#print(view())
+#insert("Apple", 6, 1.01)
+#update(9, 9.89, "Orange")
+#delete("Apple")
+print(view())
 #create_table("cake", 18, 4.79)

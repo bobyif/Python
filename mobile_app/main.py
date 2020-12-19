badd87fef3e343c7db4e74ce915167b57740d085
Boyan Fotev <bobyif.if@gmail.com>
2020-12-19 03:53:17 +0200

    Progress

--- mobile_app/main.py
+++ mobile_app/main.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+import string
 
 Builder.load_file("design.kv")
 
@@ -11,7 +12,24 @@ class LoginScreen(Screen):
 
 
 class SignUpScreen(Screen):
-    pass
+    def add_user(self, uname, pword):
+        incorrect = False
+        for symbols in string.punctuation:
+            if symbols in uname:
+                print("You can't use: !#$%&'()*+,-./:;<=>?@[\]^_`{|}~")
+                incorrect = True
+        for symbols1 in string.punctuation:
+            if symbols1 in pword:
+                print("You can't use: !#$%&'()*+,-./:;<=>?@[\]^_`{|}~")
+                incorrect = True
+        if incorrect:
+            pass
+        elif len(uname) < 5:
+            print("Your username is too short")
+        elif len(pword) < 7:
+            print("Your password is too short")
+        else:
+            print("This is the Username : {} and this is the password : {} of this user".format(uname, pword))
 
 
 class RootWidget(ScreenManager):

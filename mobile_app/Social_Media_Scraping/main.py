9de9c52f81285185b588065888811ffbbc1d0c35
Boyan Fotev <bobyif.if@gmail.com>
2021-02-07 03:09:01 +0200

    we did the scroll thing login_success screen

--- mobile_app/Social_Media_Scraping/main.py
+++ mobile_app/Social_Media_Scraping/main.py
@@ -3,8 +3,7 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
-import bs4
-import string
+from scrapper import scrapper
 from datetime import datetime
 import json
 
@@ -13,14 +12,18 @@ Builder.load_file("design.kv")
 
 
 class LoginScreen(Screen):
+
     def sign_up(self):
         self.manager.current = "sign_up_screen"
 
-    def login(self, username, pword):
+    def login(self, username, pword, person):
         with open("login.json") as file:
             user = json.load(file)
         if username in user and user[username]["password"] == pword:
-            self.manager.current = "login_success"
+            global celeburty
+            celeburty = person
+            scrapper(celeburty)
+            self.manager.current = "login_almost_success"
         else:
             self.ids.login_wrong.text = "Wrong password"
 
@@ -35,7 +38,11 @@ class SignUpScreen(Screen):
 
         with open("login.json", "w") as file:
             json.dump(user, file)
-        self.manager.current = "login_success"
+        self.manager.current = "login_almost_success"
+
+
+class LoginAlmostSuccess(Screen):
+    pass
 
 
 class LoginSuccess(Screen):

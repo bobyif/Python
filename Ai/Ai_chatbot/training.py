bfe5ef4b53217cc32f0ccf7db3de1760f85a27b5
Boyan Fotev <bobyif.if@gmail.com>
2021-07-21 16:44:02 +0300

    init

--- Ai/Ai_chatbot/training.py
+++ Ai/Ai_chatbot/training.py
@@ -8,7 +8,7 @@ import tensorflow
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import SGD
 
-lemmatizer = WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
 
 intents = json.loads(open("intents.json").read())
 
@@ -17,4 +17,24 @@ classes = []
 documents = []
 ignore_letters = ["?", "!", ".", ","]
 
-for 
+for intent in intents["intents"]:
+    for pattern in intent["patterns"]:
+        word_list = nltk.word_tokenize(pattern)
+        words.extend(word_list)
+        documents.append((word_list, intent["tag"]))
+        if intent["tag"] not in classes:
+            classes.append(intent["tag"])
+
+words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
+words = sorted(set(words))
+
+classes = sorted(set(classes))
+
+pickle.dump(words, open("words.pkl", "wb"))
+pickle.dump(words, open("classes.pkl", "wb"))
+
+training = []
+output_empty = [0] * len(classes)
+
+for document in documents:
+    s

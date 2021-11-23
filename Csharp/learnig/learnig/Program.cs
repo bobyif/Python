cbeb438eb4831faeb0f13801b490dcba14b81b65
Boyan Fotev <bobyif.if@gmail.com>
2021-11-24 00:48:53 +0200

    init

---
+++ Csharp/learnig/learnig/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Activities;
+using System.Activities.Statements;
+using System.Linq;
+
+namespace learnig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}

b88ccbed10bec008dd5a9cd03d87cf46bb5098a7
bobyif <bobyif.if@gmail.com>
2022-05-10 17:15:21 +0300

    init

--- 2018 AIPO National Finals/AIPO2018_Finals_exe4.py
+++ 2018 AIPO National Finals/AIPO2018_Finals_exe4.py
@@ -1,1 +1,8 @@
-#road = 
+n = input("Enter here : ")
+
+
+#for i in range(1,int(n)):
+#    for n in range(i + 1, int(n)):  
+#        print(n)
+#    else:
+#        print(i)
---
+++ 2018 AIPO National Finals/test.py
@@ -0,0 +1,7 @@
+i = 0
+
+for i in range(1,100):
+    for n in range (1, 11):
+        if i % n == 0:
+            print(i)
+        
